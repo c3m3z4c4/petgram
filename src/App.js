d2c0dc3f4118521d34b32c4335ee2b500bcc13ef
Cesar Meza <deorsoft@gmail.com>
2020-09-29 17:24:51 -0500

    ListOfPhotoCards

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 import { ListOfCategories } from './Components/ListOfCategories'
 import { GlobalStyle } from './GlobalStyle'
+import { ListOfPhotoCards } from './Components/ListOfPhotoCards'
 
 export const App = () => (
   <div>
     <GlobalStyle />
     <ListOfCategories />
+    <ListOfPhotoCards />
 
   </div>
 )
