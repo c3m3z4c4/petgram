625077d8bbb94ac2b008fcfd11d47d0210e73525
C3M3Z4 <cmeza@mezadigital.com>
2020-09-30 06:45:28 -0500

    sync mac => dell

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react'
 
 import { ListOfCategories } from './Components/ListOfCategories'
-import { GlobalStyle } from './GlobalStyle'
+import { GlobalStyle } from './Styles/GlobalStyle'
 import { ListOfPhotoCards } from './Components/ListOfPhotoCards'
+import { Logo } from './Components/Logo'
 
 export const App = () => (
   <div>
     <GlobalStyle />
+    <Logo/>
     <ListOfCategories />
     <ListOfPhotoCards />
-
   </div>
 )
--- src/Components/PhotoCard/styles.js
+++ src/Components/PhotoCard/styles.js
@@ -1,4 +1,6 @@
 import styled from 'styled-components'
+import { fadeIn } from '../../styles/animation'
+
 
 export const ImgWrapper = styled.div`
   border-radius: 10px;
@@ -10,6 +12,7 @@ export const ImgWrapper = styled.div`
   width: 100%; 
 `
 export const Img = styled.img`
+  ${fadeIn()}
   box-shadow: 0 10px 14px rgba(0, 0, 0, .2);
   height: 100%;
   object-fit: cover;
