fa5eff94b9397ec3592ef95243de2cb474158324
Cesar Meza <deorsoft@gmail.com>
2020-10-02 08:53:26 -0500

    Corrigiendo ruta del global style

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
 import { ListOfCategories } from './Components/ListOfCategories'
-import { GlobalStyle } from './Styles/GlobalStyle'
+import { GlobalStyle } from './styles/GlobalStyle.js'
 import { ListOfPhotoCards } from './Components/ListOfPhotoCards'
 import { Logo } from './Components/Logo'
 
 export const App = () => (
   <div>
     <GlobalStyle />
-    <Logo/>
+    <Logo />
     <ListOfCategories />
     <ListOfPhotoCards />
   </div>
