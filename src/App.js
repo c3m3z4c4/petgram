6bdeda2e1a2fbefd65683b17f32bd5480d035133
C3M3Z4 <cmeza@mezadigital.com>
2020-10-12 21:01:34 -0500

    PWA Funcional

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React, { useContext } from 'react'
+import React, { useContext, Suspense } from 'react'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
 import { NavBar } from './components/NavBar'
 
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
-import { Favs } from './pages/Favs'
+// import { Favs } from './pages/Favs'
 import { User } from './pages/User'
 import { NotRegisteredUser } from './pages/NotRegisteredUser'
-import { NotFound } from './pages/NotFound.js'
+import { NotFound } from './pages/NotFound'
 
 import { Router, Redirect } from '@reach/router'
 import { Context } from './Context'
+
+const Favs = React.lazy(() => import('./pages/Favs'))
 
 export const App = () => {
   const { isAuth } = useContext(Context)
 
   return (
-    <div>
+    <Suspense fallback={<div />}>
       <GlobalStyle />
       <Logo />
       <Router>
@@ -28,13 +30,12 @@ export const App = () => {
         {!isAuth && <NotRegisteredUser path='/login' />}
         {!isAuth && <Redirect from='/favs' to='/login' />}
         {!isAuth && <Redirect from='/user' to='/login' />}
-        {!isAuth && <Redirect from='/login' to='/' />}
-
+        {isAuth && <Redirect from='/login' to='/' />}
         <Favs path='/favs' />
         <User path='/user' />
       </Router>
 
       <NavBar />
-    </div>
+    </Suspense>
   )
 }
